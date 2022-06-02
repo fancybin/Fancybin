5921651955b53527831cefba8e87c65ed6058867
fancybin <f4ncybin@gmail.com>
2022-06-02 20:54:07 +0700

    Welcome Activity

--- app/src/main/java/com/c22ps333/fancybin/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/c22ps333/fancybin/ui/welcome/WelcomeActivity.kt
@@ -1,12 +1,54 @@
 package com.c22ps333.fancybin.ui.welcome
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.c22ps333.fancybin.R
+import com.c22ps333.fancybin.databinding.ActivityOnboardingBinding
+import com.c22ps333.fancybin.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private val binding: ActivityWelcomeBinding by lazy {
+        ActivityWelcomeBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        setContentView(binding.root)
+
+        setupView()
     }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+        //setupAction()
+    }
+
+/*
+    private fun setupAction() {
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+*/
 }
