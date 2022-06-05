8add317bb69ef29df4c5340074a0ee6d3c203c8e
fancybin <f4ncybin@gmail.com>
2022-06-05 08:26:52 +0700

    added Register Activity

---
+++ app/src/main/java/com/c22ps333/fancybin/ui/register/RegisterActivity.kt
@@ -0,0 +1,44 @@
+package com.c22ps333.fancybin.ui.register
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.c22ps333.fancybin.databinding.ActivityRegisterBinding
+import com.c22ps333.fancybin.ui.main.MainActivity
+
+class RegisterActivity : AppCompatActivity() {
+    private val binding: ActivityRegisterBinding by lazy {
+        ActivityRegisterBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
