579af913fd05c47ce236e38786aac9773526bbe7
fancybin <f4ncybin@gmail.com>
2022-06-05 10:28:45 +0700

    Added Education Fragment

--- app/src/main/java/com/c22ps333/fancybin/ui/education/EducationFragment.kt
+++ app/src/main/java/com/c22ps333/fancybin/ui/education/EducationFragment.kt
@@ -1,4 +1,4 @@
-package com.c22ps333.fancybin.ui.dashboard
+package com.c22ps333.fancybin.ui.education
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.c22ps333.fancybin.databinding.FragmentDashboardBinding
+import com.c22ps333.fancybin.databinding.FragmentEducationBinding
 
-class DashboardFragment : Fragment() {
+class EducationFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentEducationBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,9 +23,9 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this).get(EducationViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentEducationBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
--- app/src/main/java/com/c22ps333/fancybin/ui/education/EducationViewModel.kt
+++ app/src/main/java/com/c22ps333/fancybin/ui/education/EducationViewModel.kt
@@ -1,13 +1,13 @@
-package com.c22ps333.fancybin.ui.dashboard
+package com.c22ps333.fancybin.ui.education
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class EducationViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is Education Fragment"
     }
     val text: LiveData<String> = _text
 }
