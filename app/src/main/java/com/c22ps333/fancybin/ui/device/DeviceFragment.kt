59e24968336fb9d0cfa2c3cab267146cbb2cf79f
fancybin <f4ncybin@gmail.com>
2022-06-06 14:16:06 +0700

    Added Device location fragment

--- app/src/main/java/com/c22ps333/fancybin/ui/device/DeviceFragment.kt
+++ app/src/main/java/com/c22ps333/fancybin/ui/device/DeviceFragment.kt
@@ -1,42 +1,100 @@
 package com.c22ps333.fancybin.ui.device
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.fragment.app.Fragment
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.c22ps333.fancybin.databinding.FragmentDeviceBinding
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.c22ps333.fancybin.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
 class DeviceFragment : Fragment() {
 
-    private var _binding: FragmentDeviceBinding? = null
+    private lateinit var mMap: GoogleMap
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        mMap = googleMap
+
+        mMap.uiSettings.apply {
+            isZoomControlsEnabled = true
+            isCompassEnabled = true
+            isMapToolbarEnabled = true
+        }
+
+        getDeviceLocation()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val deviceViewModel =
-            ViewModelProvider(this).get(DeviceViewModel::class.java)
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_device, container, false)
+    }
 
-        _binding = FragmentDeviceBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        val textView: TextView = binding.textNotifications
-        deviceViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+    }
+
+    private fun getDeviceLocation() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext().applicationContext,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.isMyLocationEnabled = true
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    val latLng = LatLng(location.latitude, location.longitude)
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 8f))
+                } else {
+                    Toast.makeText(requireContext(), getString(R.string.location_off_message), Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
         }
-        return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                getDeviceLocation()
+            }
+        }
+
 }
