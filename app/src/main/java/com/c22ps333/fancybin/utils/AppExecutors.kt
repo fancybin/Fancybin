3d9ac8c9267edcfa2f59a4153eb6e212246815ab
fancybin <f4ncybin@gmail.com>
2022-06-07 08:13:05 +0700

    Added AppExecutors

---
+++ app/src/main/java/com/c22ps333/fancybin/utils/AppExecutors.kt
@@ -0,0 +1,8 @@
+package com.c22ps333.fancybin.utils
+
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+class AppExecutors {
+    val diskIO: Executor = Executors.newSingleThreadExecutor()
+}
