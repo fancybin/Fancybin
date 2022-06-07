99c434d9bff2eaa7f6d87efd62fe03ced8622640
fancybin <f4ncybin@gmail.com>
2022-06-07 08:19:31 +0700

    Scan fixed

--- app/src/main/java/com/c22ps333/fancybin/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/c22ps333/fancybin/ui/scan/ScanFragment.kt
@@ -1,17 +1,44 @@
 package com.c22ps333.fancybin.ui.scan
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import androidx.exifinterface.media.ExifInterface
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.c22ps333.fancybin.databinding.FragmentScanBinding
+import com.c22ps333.fancybin.utils.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+
 
 class ScanFragment : Fragment() {
 
     private var _binding: FragmentScanBinding? = null
+
+    private lateinit var currentPhotoPath: String
+    private var getFile: File? = null
+    private var reducingDone: Boolean = false
+
+    private val appExecutor: AppExecutors by lazy {
+        AppExecutors()
+    }
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -20,23 +47,138 @@ class ScanFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
-        val scanViewModel =
-            ViewModelProvider(this).get(ScanViewModel::class.java)
+        _binding = FragmentScanBinding.inflate(LayoutInflater.from(requireActivity()))
+        return binding?.root
+    }
 
-        _binding = FragmentScanBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        val textView: TextView = binding.textNotifications
-        scanViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        setupButtons()
+    }
+
+    private fun setupButtons() {
+        binding.btnCamera.setOnClickListener {
+            if (!allPermissionsGranted()) {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    REQUIRED_PERMISSIONS,
+                    REQUEST_CODE_PERMISSIONS
+                )
+            }
         }
-        return root
+
+        binding.btnCamera.setOnClickListener { takePhoto() }
+        binding.btnGallery.setOnClickListener { startGallery() }
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private fun takePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        activity?.let {
+            intent.resolveActivity(it.packageManager)
+
+            createTempFile(it.application).also {
+                val photoURI: Uri = FileProvider.getUriForFile(
+                    requireActivity(),
+                    "com.c22ps333.fancybin",
+                    it
+                )
+                currentPhotoPath = it.absolutePath
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                launcherIntentCamera.launch(intent)
+            }
+        }
+    }
+
+
+
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == AppCompatActivity.RESULT_OK) {
+
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = fromUriToFile(selectedImg, requireActivity())
+
+
+            getFile = myFile
+
+            val result = BitmapFactory.decodeFile(getFile?.path)
+            result.compress(Bitmap.CompressFormat.JPEG, 100, FileOutputStream(getFile))
+
+            appExecutor.diskIO.execute {
+                getFile = reduceFileImage(getFile as File)
+                reducingDone = true
+            }
+            binding.ivImage.setImageURI(selectedImg)
+        }
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == AppCompatActivity.RESULT_OK) {
+            val myFile = File(currentPhotoPath).also { getFile = it }
+            val os: OutputStream
+
+            // Rotate image to correct orientation
+            val bitmap = BitmapFactory.decodeFile(getFile?.path)
+            val exif = ExifInterface(currentPhotoPath)
+            val orientation: Int = exif.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_UNDEFINED
+            )
+
+            val rotatedBitmap: Bitmap = when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> TransformationUtils.rotateImage(bitmap, 90)
+                ExifInterface.ORIENTATION_ROTATE_180 -> TransformationUtils.rotateImage(bitmap, 180)
+                ExifInterface.ORIENTATION_ROTATE_270 -> TransformationUtils.rotateImage(bitmap, 270)
+                ExifInterface.ORIENTATION_NORMAL -> bitmap
+                else -> bitmap
+            }
+
+
+
+            // Convert rotated image to file
+            try {
+                os = FileOutputStream(myFile)
+                rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, os)
+                os.flush()
+                os.close()
+
+                getFile = myFile
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+
+            binding.ivImage.setImageBitmap(rotatedBitmap)
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
     }
 }
