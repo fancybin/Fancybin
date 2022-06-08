2ab251bc3a2df8bdf7f00072bcf34710f6a09cdb
fancybin <f4ncybin@gmail.com>
2022-06-08 19:27:59 +0700

    import optimise

--- app/src/main/java/com/c22ps333/fancybin/ui/model/UserPreferences.kt
+++ app/src/main/java/com/c22ps333/fancybin/ui/model/UserPreferences.kt
@@ -4,7 +4,6 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.*
 
 
